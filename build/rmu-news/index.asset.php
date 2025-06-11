fca9d18bdf9ab05b7e04c7664a255903626fcb3f
parich suriya <parich.suriya@gmail.com>
2025-06-11 15:45:03 +0700

    first start

---
+++ build/rmu-news/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '60cffde85f3496fe4594');
---
+++ build/rmu-news/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '49dea46db7776f1b0844');
